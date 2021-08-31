3000b8fc7036f6bbc96083f0bf1a161b9793181d
sgash708 <sugash7081023@gmail.com>
2021-08-31 13:08:10 +0900

    controllerにCRUD追記

--- nest-tutorial/src/src/item/item.controller.ts
+++ nest-tutorial/src/src/item/item.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param
+} from '@nestjs/common';
+import { ItemService } from './item.service';
+import { Item } from '../entities/item.entity';
+import { CreateItemDTO } from './item.dto';
+import { InsertResult, UpdateResult, DeleteResult } from "typeorm";
 
 @Controller('item')
-export class ItemController {}
+export class ItemController {
+    // ItemServiceの呼び出し
+    constructor(private readonly service: ItemService) {}
+
+    // getItemList 一覧取得
+    @Get()
+    async getItemList(): Promise<Item[]> {
+        return await this.service.findAll();
+    }
+
+    // addItem データ新規登録
+    @Post()
+    async addItem(@Body() item: CreateItemDTO): Promise<InsertResult> {
+        return await this.service.create(item);
+    }
+
+    // getItem ID指定して取得
+    @Get(':id')
+    async getItem(@Param('id') id: string): Promise<Item> {
+        return await this.service.find(Number(id));
+    }
+
+}
