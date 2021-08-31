cda7466cde105c97ca78cca9982f80a7dd402b42
sgash708 <sugash7081023@gmail.com>
2021-08-31 12:20:38 +0900

    CRUDロジック追記

--- nest-tutorial/src/src/item/item.service.ts
+++ nest-tutorial/src/src/item/item.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from 'src/entities/item.entity';
-import { Repository } from 'typeorm';
+import { Repository, InsertResult, UpdateResult, DeleteResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateItemDTO } from './item.dto';
 
 
 @Injectable()
@@ -11,4 +12,29 @@ export class ItemService {
         @InjectRepository(Item)
         private readonly itemRepository: Repository<Item>
     ) {}
+
+    // findAll 全データ取得
+    async findAll(): Promise<Item[]> {
+        return await this.itemRepository.find();
+    }
+
+    // create データ1件追加
+    async create(item: CreateItemDTO): Promise<InsertResult> {
+        return await this.itemRepository.insert(item);
+    }
+
+    // find データ1件取得
+    async find(id: number): Promise<Item> | null {
+        return await this.itemRepository.findOne({id: id});
+    }
+
+    // update データ1件取得
+    async update(id: number, item): Promise<UpdateResult> {
+        return await this.itemRepository.update(id, item);
+    }
+
+    // delete データ1件削除
+    async delete(id: number): Promise<DeleteResult> {
+        return await this.itemRepository.delete(id);
+    }
 }
