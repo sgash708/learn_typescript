c85887aa69dd66d5a1da1f4c21fab309ba0c8309
sgash708 <sugash7081023@gmail.com>
2021-08-31 11:52:52 +0900

    Repository注入を記述

--- nest-tutorial/src/src/item/item.service.ts
+++ nest-tutorial/src/src/item/item.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from 'src/entities/item.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
 
 @Injectable()
-export class ItemService {}
+export class ItemService {
+    constructor(
+        // DIコンテナが登録されたRepositoryを生成して渡してくれる
+        @InjectRepository(Item)
+        private readonly itemRepository: Repository<Item>
+    ) {}
+}
