f65e65550cd2c7b4a5337851ddca937ff06c04cf
sgash708 <sugash7081023@gmail.com>
2021-08-31 14:41:12 +0900

    不要なServiceとController削除

--- nest-tutorial/src/src/app.module.ts
+++ nest-tutorial/src/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemModule } from './item/item.module';
 
 @Module({
   // 全てのモジュールからTypeORMが呼び出し可能
   imports: [TypeOrmModule.forRoot(), ItemModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
