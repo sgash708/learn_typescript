dca7cdf40fae0d9ba67ae5b738a4471bc29b6792
sgash708 <sugash7081023@gmail.com>
2021-11-02 14:47:16 +0900

    数値型の使い分けまで

--- fa-tutorial/src/lesson2/primitive.ts
+++ fa-tutorial/src/lesson2/primitive.ts
@@ -73,6 +73,11 @@ console.log(parseInt("777", 8));
 
 console.log(Number(true)); // 1
 
+// number は２進数なので0.3のように表示ができないので「decimal.js」を使用すること
+const a = 0.1;
+const b = 0.2;
+console.log(a + b); // 0.30000000000000004
+
 /**
  * stringリテラル
  */
