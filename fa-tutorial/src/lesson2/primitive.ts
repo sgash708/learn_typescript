e056b919542c972ffc8f0935b3dc99b25b856d9d
sgash708 <sugash7081023@gmail.com>
2021-11-02 13:13:32 +0900

    2/4/8/16進数について

--- fa-tutorial/src/lesson2/primitive.ts
+++ fa-tutorial/src/lesson2/primitive.ts
@@ -20,19 +20,46 @@ console.log(String(flg)); // 'true'
 console.log(Number(flg)); // 1
 
 // 他タイプからbooleanにキャストする方法
-const notEmpty = Boolean("test string");
+const notEmpty: boolean = Boolean("test string");
 // 文字列(true)同士の比較結果をBooleanで取る
-const flagStr = 'true';
-const castFlag = flagStr === 'true';
+const flagStr: string = 'true';
+const castFlag: boolean = flagStr === 'true';
 // 反転について
-const str = 'test';
-const isEmpty = !str;
+const str: string = 'test';
+const isEmpty: boolean = !str;
 console.log(str, isEmpty);
+
+// ド・モルガンの法則
+
+// CASE1: "false && false(ff)"は、"どちらかがtrueじゃない時"に通る
+// !(P || Q) == !P && !Q
+
+// CASE2: "どちらからtrueになるとき(tf/ft/ff)"は、両方ともtrueじゃない場合
+// !(P && Q) == !P || !Q
 
 /**
  * 数値型
+ * → "number"と"bigInt"の2種類存在する。
  */
+// ts/jsは「64ビットfloat」で扱う
+// → 整数をロスなく格納できるのは、53ビット(+-9007兆)まで扱える
+// → それ以上の数値は末尾が誤差としてカットされるので、整数を期待して扱うと問題が出る場合がある。
+// 正確な上限/下限は、下記定数で確認できる。
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+// 範囲内で数値が収まっているか確認できる
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1));
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER));
 
+console.log(10.5);
+console.log(128);
+console.log(0b11);  // 0b => binary
+console.log(0o777); // 0o => octal
+console.log(0xf7);  // 0x => hexial
+
+// 0o => octal 8進数だから、512にするには値を繰り上げた1000となる
+// REF: http://cute.sh/solairo/binary/02.html
+console.log(0o1000);
 
 /**
  * stringリテラル
